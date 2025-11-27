95f9cd26e0e50b54f3a8d7183afe9f2b5d78211e
Khalid Shams <khalidshams108@gmail.com>
2025-11-27 02:18:49 +0000

    adds Hal program

---
+++ hal-9000.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#A script to demo  user input and variabke use 
+
+
+
+echo "Hello. I'm HAL 9000. What is your name?"
+
+read USERNAME
+
+echo "I'm sorry $USERNAME. I'm afraid I can't open the pod bay doors"
+
+ 
